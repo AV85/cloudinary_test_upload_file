d0e969f85be7bec70954e689607ebaf3dde1f222
Oleksandr Vykhor <o.vykhor@scrumlaunch.com>
2024-01-10 13:33:53 +0100

    update conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "cloudinary-test-upload-file.onrender.com"
 end
